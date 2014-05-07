0da600ed2599f9d06aedb9bbacc5afb35668b4bc
lulalachen <lulalachen@lulalachens-mbp.home>
2014-05-08 00:00:16 +0800

    test

--- api/controllers/FetchDataController.js
+++ api/controllers/FetchDataController.js
@@ -22,11 +22,34 @@ module.exports = {
    * Action blueprints:
    *    `/fetchdata/new`
    */
-   new: function (req, res) {
+   'search': function (req, res) {
     
-    // Send a JSON response
-    return res.json({
-      hello: 'world'
+    function download(url, callback) {
+    http.get(url, function(res) {
+        var data = "";
+        res.on('data', function (chunk) {
+          data += chunk;
+        });
+        res.on("end", function() {
+        callback(data);
+        });
+      }).on("error", function() {
+        callback(null);
+      });
+    }
+    download(url, function(data) {
+      var output;
+      if (data) {
+        //console.log(data + "*");
+        var $ = cheerio.load(data);
+        //console.log($('div[class=tab]').html());
+        res.write("<html><head><meta charset='UTF-8'/></head>");
+        res.write("<body>");
+        res.write($('.tab').html());
+        res.write("</body></html>");
+        console.log("done");
+    }
+      else console.log("error");  
     });
   },
 
